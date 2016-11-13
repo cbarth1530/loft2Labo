70ade6da5e57e5865536645369034fb3ef6ad68d
Firepolo <anthony.pasche@gmail.com>
2016-11-13 18:05:04 +0100

    Update GerardTime.h
    - Try to resolve error from indentation code in github.

--- libraries/GerardTime.h
+++ libraries/GerardTime.h
@@ -40,8 +40,8 @@ public:
     {
         addRepeat(id, SECS_PER_DAY, toSecond(hrs, min, sec), listener);
     }
-    
-    static void alarmRepeat(uint8_t id, int8_t day, uint8_t hrs, uint8_t min, uint8_t sec, AlarmListener listener)
+	
+	static void alarmRepeat(uint8_t id, int8_t day, uint8_t hrs, uint8_t min, uint8_t sec, AlarmListener listener)
     {
         addRepeat(id, SECS_PER_WEEK, day + toSecond(hrs, min, sec), listener);
     }
